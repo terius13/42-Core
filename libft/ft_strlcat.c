c8a85ddaf610babdf10a18d7c2fe0258a16059f9
Timberly Ng <ting@c2r3s4.42singapore.sg>
2023-09-12 23:02:15 +0800

    added strchr, strlcat, strlcpy to libft

---
+++ libft/ft_strlcat.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ting <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/11 18:10:00 by ting              #+#    #+#             */
+/*   Updated: 2023/09/11 22:49:41 by ting             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	srclen;
+	size_t	i;
+	size_t	j;
+
+	srclen = ft_strlen(src);
+	i = 0;
+	j = 0;
+	while (i < size && dst[i] != '\0')
+		i++;
+	if (i == size)
+		return (i + srclen);
+	else
+	{	
+		while (i + j + 1 < size && src[i] != '\0')
+		{
+			dst[i + j] = src[j];
+			j++;
+		}
+		if (i + j < size)
+			dst[i + j] = '\0';
+	}
+	return (i + srclen);
+}
+/*
+#include <string.h>
+int	main(void)
+{
+	char	string[] = "hello";
+	char	dest[] = "bye";
+	size_t	size = 10;
+
+	printf("%zu", ft_strlcat(dest, string, size));
+}
+*/
---
+++ libft/ft_strlcpy.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ting <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/11 13:57:20 by ting              #+#    #+#             */
+/*   Updated: 2023/09/11 22:11:20 by ting             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < size - 1 && src[i] != '\0')
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (ft_strlen(src));
+}
+/*
+int	main(void)
+{
+	const char	string[] = "hello";
+	char	dest[6];
+	size_t	size = 5;
+
+	printf("%zu", ft_strlcpy(dest, string, size));
+
+	return (0);
+}
+*/
