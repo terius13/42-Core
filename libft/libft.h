ea61af140a61afa6c32d6052eca24a809d63734f
Timberly Ng <ting@c2r3s4.42singapore.sg>
2023-09-12 23:04:36 +0800

    added libft.h

---
+++ libft/libft.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ting <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/10 20:13:55 by ting              #+#    #+#             */
+/*   Updated: 2023/09/10 20:13:58 by ting             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+#include <string.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <unistd.h>
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
+size_t	ft_strlen(const char *s);
+void	*ft_memset(void *s, int c, size_t n);
+void	bzero(void *s, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+int	ft_toupper(int c);
+int	ft_tolower(int c);
+int	ft_atoi(int *str);
+
+#endif
